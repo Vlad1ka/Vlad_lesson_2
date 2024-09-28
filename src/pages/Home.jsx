278dae948e2fe6128b3126c53864e751354bd374
Vlad1ka <vlad-litvinov-2020@list.ru>
2024-09-28 12:46:38 +0500

    Pagination

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import PizzaBlock from '../components/PizzaBlock';
 import Skeleton from '../components/PizzaBlock/Skeleton';
 import Categories from '../components/Categories';
-import Sort from '../components/PizzaBlock/Sort';
+import Sort from '../components/Sort';
+import Pagination from '../components/Pagination';
 
- const Home = () => {
+ const Home = ({ searchValue }) => {
     const [items, setItems]=React.useState([]);
     const [isLoading, setIsLoading] = React.useState(true);
     const [categoryId, setCategoryId] = React.useState(0);
+    const [currentPage, setCurrentPage] = React.useState(1);
     const [sortType, setSortType] = React.useState({
       name: "популярности",
       sortProperty: "rating",
@@ -19,8 +21,9 @@ import Sort from '../components/PizzaBlock/Sort';
     const sortBy = sortType.sortProperty.replace('-', '');
     const order = sortType.sortProperty.includes('-') ? 'asc' : 'desc';
     const category = categoryId > 0 ? `category=${categoryId}` : '';
+    const search = searchValue ? `&search=${searchValue}` : "";
 
-    fetch(`https://66f121e741537919154fa987.mockapi.io/Items?${category}&sortBy=${sortBy}&order=${order}`)
+    fetch(`https://66f121e741537919154fa987.mockapi.io/Items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${search}`)
 
       .then((res) => {
         return res.json();
@@ -30,7 +33,11 @@ import Sort from '../components/PizzaBlock/Sort';
         setIsLoading(false);
       });
     window.scrollTo(0, 0);
-  },[categoryId, sortType]);
+  },[categoryId, sortType, searchValue, currentPage]);
+
+  const pizzas = items.map((obj) => <PizzaBlock key={obj.id} {...obj}/>);
+
+  const skeletons = [... new Array(6)].map((_, index) => (<Skeleton key={index} />));
 
   return (
     <div className='container'>
@@ -42,12 +49,8 @@ import Sort from '../components/PizzaBlock/Sort';
         <Sort value={sortType} onChangeSort={(index) => setSortType (index)} />
       </div>
         <h2 className="content__title">Все пиццы</h2>
-        <div className="content__items">
-            {isLoading
-              ? [... new Array(6)].map((_, index) => <Skeleton key={index}/>)
-              : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)
-            }
-        </div>
+        <div className="content__items">{isLoading ? skeletons : pizzas}</div>
+        <Pagination onChangePage={(number) => setCurrentPage(number)}/>
     </div>
   )
 }
